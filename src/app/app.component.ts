9616b2c1d225d635b484512f3fce65ed62d28e53
Joao <joaohenrique9021@gmail.com>
2024-02-13 18:29:18 -0300

    final datails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Location } from './types/location.interface';
+import { GetUnitsService } from './services/get-units.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'desafio-smartfit';
+  showList = new BehaviorSubject(false)
+  unitsLits: Location[] = [];
+
+  constructor (private unitService: GetUnitsService) {
+
+  }
+
+  onSubmit() {
+    this.showList.next(true)
+    this.unitsLits = this.unitService.getFilteredUnits();
+  }
 }
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,43 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { WeekDay } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { FilterUnitsService } from 'src/app/services/filter-units.service';
 import { GetUnitsService } from 'src/app/services/get-units.service';
 import { Location } from 'src/app/types/location.interface';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent implements OnInit {
+  @Output() submitEvent = new EventEmitter();
   results: Location[] = [];
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private unitServices: GetUnitsService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitServices: GetUnitsService,
+    private filterUnitsServices: FilterUnitsService
+  ) { }
 
-  }
 
-  ngOnInit(): void{
-    this.unitServices.getAllUnits().subscribe(data=>{
-      this.results = data.locations;
-      this.filteredResults = data.locations;
-    })
+  ngOnInit(): void {
+
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false
-    })
+      showClosed: true
+    });
+    this.unitServices.getAllUnits().subscribe((data) => {
+      this.results = data;
+      this.filteredResults = data;
+    });
   }
 
-  onSubmit(): void{
-    if(!this.formGroup.value.showClosed){
-      this.filteredResults = this.results.filter(location => location.opened === true);
-    } else {
-      this.filteredResults = this.results;
-    }
+  onSubmit(): void {
+    let { showClosed, hour } = this.formGroup.value;
+    this.filteredResults = this.filterUnitsServices.filter(this.results, showClosed, hour);
+    this.unitServices.setFilteredUnits(this.filteredResults)
+
+    this.submitEvent.emit();
   }
 
-  onClean(): void{
-   this.formGroup.reset()
+  onClean(): void {
+    this.formGroup.reset();
   }
-
 }
